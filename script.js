2f62fab6b362ceecbec36898a1ca316ec6f21095
pranjal641 <120087246+pranjal641@users.noreply.github.com>
2023-10-03 16:18:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // write js code here if required
+ function updateTimer() {
+      // Get the current date and time
+      var currentDate = new Date();
+
+      // Format the date and time as a string
+      var dateString = currentDate.toLocaleString();
+
+      // Update the timer display
+      var timerElement = document.getElementById("timer");
+      timerElement.textContent = dateString;
+    }
+
+    // Call the updateTimer function every second
+    setInterval(updateTimer, 1000);
